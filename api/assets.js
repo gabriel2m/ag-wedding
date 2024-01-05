f4d87d1d03532a7b7baac45bfe64e8cd641f404a
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-05 17:36:13 -0300

    fix: update paths

--- api/assets.js
+++ api/assets.js
@@ -1,6 +1,7 @@
+import path from 'path';
 import express from "express";
 const app = express();
 
-app.use('/assets', express.static(__dirname + '/public'));
+app.use('/assets', express.static(path.resolve() + '/public'));
 
 export default app;
