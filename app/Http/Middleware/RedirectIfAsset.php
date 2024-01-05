0fb0ed5a59cbfbabc1e693c91903d0221e9f78ce
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-05 16:29:40 -0300

    feat: make assets accessible on vercel

---
+++ app/Http/Middleware/RedirectIfAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfAsset
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (File::exists(public_path($request->path()))) {
+            return redirect('assets');
+        }
+
+        return $next($request);
+    }
+}
