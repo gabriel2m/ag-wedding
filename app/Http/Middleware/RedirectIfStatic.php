5efcbfca19eb37abd6ccec8b6ed6f55503905441
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-05 19:02:22 -0300

    style: clear code

--- app/Http/Middleware/RedirectIfStatic.php
+++ app/Http/Middleware/RedirectIfStatic.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,7 +12,10 @@ class RedirectIfStatic
     public function handle(Request $request, Closure $next): Response
     {
         if (File::isFile(public_path($request->path()))) {
-            return redirect(to: '/static/' . $request->path(), secure: true);
+            return redirect(
+                to: '/static/' . $request->path(),
+                secure: true
+            );
         }
 
         return $next($request);
