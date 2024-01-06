fc2b207dfd40055a62306f58c8718b7c41eba130
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-05 22:57:52 -0300

    fix: add pgsql endpoint id on connection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Connectors\PostgresConnector;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('db.connector.pgsql', function () {
+            return new class extends PostgresConnector {
+                protected function getDsn(array $config)
+                {
+                    $dsn = parent::getDsn($config);
+
+                    if (!config('database.connections.pgsql.add_endpoint_id')) {
+                        return $dsn;
+                    }
+
+                    return $dsn . ';options=endpoint=' . Str::before($config['host'], '.');
+                }
+            };
+        });
     }
 
     /**
