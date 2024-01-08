d5d72bd579829051e172d0fd67f90a60740591d8
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-08 19:03:17 -0300

    build: add tailwind

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,15 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     @php
         $title[] = config('app.name');
     @endphp
     <title>{{ implode(' | ', $title) }}</title>
+
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
     @vite('resources/css/app.css')
     @stack('styles')
