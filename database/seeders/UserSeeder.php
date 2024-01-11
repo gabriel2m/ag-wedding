19f30eb8a3282f8c6653a1da4976aefac154f1f3
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-10 22:43:02 -0300

    feat: add User.seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'gabriel2m',
+            'email' => 'gabriel2m.contact@gmail.com',
+            'password' => '12345',
+        ]);
+    }
+}
