5cc15d92224263ab5e300211cbe112acfaa5ab70
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-20 11:45:55 -0300

    style: pint and prettier format

--- resources/views/components/auth/button.blade.php
+++ resources/views/components/auth/button.blade.php
@@ -1,1 +1,3 @@
-<x-button :attributes="$attributes->class(['flex w-full justify-center rounded-md bg-slate-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'])" />
+<x-button :attributes="$attributes->class([
+    'flex w-full justify-center rounded-md bg-slate-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+])" />
