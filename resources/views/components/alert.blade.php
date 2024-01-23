f3532c082e738781c4d1bba2536aa27e2169ffe9
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-23 18:05:38 -0300

    refactor: clear alert

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -2,17 +2,16 @@
 
 @if ($message)
     <div x-data="{ open: true }">
-        <div {{ $attributes->class([
-            'my-4 w-full border-l-4',
-            match ($type) {
-                'success' => 'border-green-600 bg-green-200 p-4 hover:border-green-500',
-                'error' => 'border-red-600 bg-red-200 p-4 hover:border-red-500',
-                'warning' => 'border-yellow-600 bg-yellow-200 p-4 hover:border-yellow-500',
-                'info' => 'border-blue-600 bg-blue-200 p-4 hover:border-blue-500',
-            },
-        ]) }}
-            x-show="open" x-transition:leave="transition ease-in duration-300"
-            x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-90">
+        <div x-show="open" x-transition.duration.600ms
+            {{ $attributes->class([
+                'my-4 w-full border-l-4',
+                match ($type) {
+                    'success' => 'border-green-600 bg-green-200 p-4 hover:border-green-500',
+                    'error' => 'border-red-600 bg-red-200 p-4 hover:border-red-500',
+                    'warning' => 'border-yellow-600 bg-yellow-200 p-4 hover:border-yellow-500',
+                    'info' => 'border-blue-600 bg-blue-200 p-4 hover:border-blue-500',
+                },
+            ]) }}>
             <div @class([
                 'flex justify-between items-center',
                 match ($type) {
