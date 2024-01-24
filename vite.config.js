7380df1dd29fc36ca0d803426a03640b03597654
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-24 16:59:05 -0300

    refactor: use glob to list assets

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { globSync } from "glob";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: globSync("resources/{css,js}/**/*.*", {
+                ignore: "resources/js/bootstrap.js",
+            }),
             refresh: true,
         }),
     ],
