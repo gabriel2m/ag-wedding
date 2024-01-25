87032ad3202f5bbb25fa3e6c007f1c737d5e8b54
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-25 15:53:37 -0300

    chore: show vercel php info

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/info', function () {
+    phpinfo();
+});
+
 Route::get('/', function () {
     return to_route('admin.home');
 })->name('home');
