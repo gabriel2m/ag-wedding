3fcf19f620f98acd1fc1b60a62238ec1b991b82b
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-26 16:43:00 -0300

    style: prettier format

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-col h-full px-6">
-        <div class="w-full sm:max-w-sm m-auto">
+    <div class="flex h-full flex-col px-6">
+        <div class="m-auto w-full sm:max-w-sm">
             <x-alert message="{{ session('status') }}" />
 
             @yield('content')
