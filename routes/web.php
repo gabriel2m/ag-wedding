b6cb08b3a77f3f32533cd1de7e30c8b34d0d21de
gabriel2m <gabriel2m.contact@gmail.com>
2024-01-26 18:26:07 -0300

    Revert "chore: show vercel php info"
    This reverts commit 87032ad3202f5bbb25fa3e6c007f1c737d5e8b54.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/info', function () {
-    phpinfo();
-});
-
 Route::get('/', function () {
     return to_route('admin.home');
 })->name('home');
