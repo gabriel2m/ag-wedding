35c61071813fb50d63fc43a0c6006eb9e369994f
gabriel2m <gabriel2m.contact@gmail.com>
2024-02-06 16:14:58 -0300

    feat: add colspan all

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,26 @@
 import "./bootstrap";
 
-htmx.on("htmx:beforeSwap", function (evt) {
+/**
+ * Render error response
+ */
+htmx.on("htmx:beforeSwap", (evt) => {
     if (`${evt.detail.xhr.status}`[0] != 2) {
         evt.detail.shouldSwap = true;
         evt.detail.target = htmx.find("body");
     }
+});
+
+/**
+ * colspan="all"
+ */
+htmx.on("htmx:afterSwap", () => {
+    document.querySelectorAll('[colspan="all"]').forEach((el) => {
+        let cols = 0;
+        el.closest("table")
+            .querySelectorAll("tr")
+            .forEach((tr) => {
+                cols = Math.max(cols, tr.querySelectorAll("td, th").length);
+            });
+        el.setAttribute("colspan", cols);
+    });
 });
