6d7a4020a3e1ddfb91def50da1c39e754115d11b
gabriel2m <gabriel2m.contact@gmail.com>
2024-02-17 13:08:48 -0300

    refactor: loading color current

--- resources/views/components/admin/loading.blade.php
+++ resources/views/components/admin/loading.blade.php
@@ -1,1 +1,1 @@
-<x-icon-spinner :attributes="$attributes->class(['loading m-auto animate-spin fill-current text-slate-400'])" />
+<x-icon-spinner :attributes="$attributes->class(['loading m-auto animate-spin fill-current'])" />
